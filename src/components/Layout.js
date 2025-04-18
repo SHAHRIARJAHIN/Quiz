85e40264963ab34ef31a1c9128fc6c96f9f30d04
Shahriar Morshed Jahin <57398824+SHAHRIARJAHIN@users.noreply.github.com>
2025-04-18 15:29:09 +0600

    Add files via upload

---
+++ src/components/Layout.js
@@ -0,0 +1,29 @@
+// src/components/Layout.js
+import React from 'react';
+import './Layout.css';
+import eventLogo from '../assets/event-logo.png'; // You'll need to add your logo file
+import sponsorLogo from '../assets/sponsor-logo.png'; // You'll need to add your logo file
+import organizerLogo from '../assets/organizer-logo.png';
+function Layout({ children }) {
+  return (
+    <div className="layout-container">
+      <header className="app-header">
+      <img src={organizerLogo} alt="Event Logo" className="event-logo" />
+      <img src={eventLogo} alt="Event Logo" className="event-logo" />
+        <img src={sponsorLogo} alt="Event Logo" className="event-logo" />
+      </header>
+      
+      <main className="app-content">
+        {children}
+      </main>
+      
+      <footer className="app-footer">
+        <div className="powered-by">
+          Powered by <img src={sponsorLogo} alt="Sponsor Logo" className="sponsor-logo" />
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Layout;
