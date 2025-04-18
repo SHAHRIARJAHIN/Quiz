91f9a537be98b76a7b66b80baa19fc917a66ab85
Shahriar Morshed Jahin <57398824+SHAHRIARJAHIN@users.noreply.github.com>
2025-04-18 16:24:39 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+// Dynamically set basename based on host (GitHub Pages vs Vercel)
+const getBaseName = () => {
+  return window.location.hostname === 'shahriarjahin.github.io' ? '/quiz' : '/';
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={getBaseName()}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
